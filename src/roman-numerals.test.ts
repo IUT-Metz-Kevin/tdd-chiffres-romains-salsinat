5aae306ad81f227c466ff185b475b2699f2c4b40
stanislas <stanislas.guilleray9@etu.univ-lorraine.fr>
2025-05-23 11:03:08 +0200

    RED

--- src/roman-numerals.test.ts
+++ src/roman-numerals.test.ts
@@ -1,1 +1,15 @@
-// Write your test here
+import { it, expect, test } from "vitest";
+
+class RomanNumeral {
+    of(value: string): number {
+        return 0;
+    }
+}
+
+const r = new RomanNumeral
+
+test.each([
+    ['I', 1],
+])("%s should return %s",(input, expected) => {
+    expect(r.of(input[0])).toBe(input[1]);
+});
