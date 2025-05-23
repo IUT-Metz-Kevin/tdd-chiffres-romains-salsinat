a3e721dc6c1b3bc97b15251e255bfa288f12d36a
stanislas <stanislas.guilleray9@etu.univ-lorraine.fr>
2025-05-23 11:10:18 +0200

    RED

--- src/roman-numerals.test.ts
+++ src/roman-numerals.test.ts
@@ -13,6 +13,7 @@ const r = new RomanNumeral
 test.each([
     ['I', 1],
     ['II', 2],
+    ['III', 3],
 ])("%s should return %s",(input, expected) => {
     expect(r.of(input)).toBe(expected);
 });
